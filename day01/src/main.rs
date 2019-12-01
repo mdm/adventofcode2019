d32c9c6f3a4fe8473acaefcc4e2cf52952d58131
Marc Dominik Migge <marcmigge@gmx.net>
2019-12-01 13:03:26 +0100

    Solution for day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let mut total_fuel = 0;
+    let mut total_additional_fuel = 0;
+    for line in std::io::stdin().lock().lines() {
+        let mass: i32 = line.unwrap().parse().unwrap();
+
+        let fuel = mass / 3 - 2;
+        total_fuel += fuel;
+
+        let mut additional_fuel = fuel / 3 - 2;
+        while additional_fuel > 0 {
+            total_additional_fuel += additional_fuel;
+            additional_fuel = additional_fuel / 3 - 2;
+        }
+    }
+
+    println!("{}", total_fuel);
+    println!("{}", total_fuel + total_additional_fuel);
 }
